ba8fa9f0f4e4019d1c65b32c80a54e3a960f3445
slrondom <sl.rondon.m@gmail.com>
2022-09-02 16:05:47 -0500

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import "./assets/scss/styles.scss";
 
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+
+createApp(App).mount("#app");
