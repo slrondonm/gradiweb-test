2d658000ad337ec7f8d42b6dc455b5a086fc3851
slrondom <sl.rondon.m@gmail.com>
2022-09-02 17:17:33 -0500

    Agrgado axios y prueba del endpoint suministrado

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,26 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'App',
+  data() {
+    return {
+      product: []
+    }
+  },
   components: {
+  },
+  methods: {
+    async getProduct() {
+      const response = await axios.get('https://graditest-store.myshopify.com/products/free-trainer-3-mmw.js')
+      this.product = response.data
+      console.log(this.product)
+    }
+  },
+  async mounted() {
+    this.getProduct()
   }
 }
 </script>
